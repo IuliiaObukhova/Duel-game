a1782a0038decde31413498380f47b94eab92d7e
IuliiaObukhova <145312042+IuliiaObukhova@users.noreply.github.com>
2024-09-01 12:48:15 -0400

    Updates

---
+++ precache-manifest.56b2aa02a38cc5bffd6172ccd024b879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa074019752acbe22925f1e004114b8",
+    "url": "/duel-game/index.html"
+  },
+  {
+    "revision": "05fa04e68b3ffe746a42",
+    "url": "/duel-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "531e83375c64121ba1cb",
+    "url": "/duel-game/static/js/2.f5c5f19a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/duel-game/static/js/2.f5c5f19a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fa04e68b3ffe746a42",
+    "url": "/duel-game/static/js/main.e28ecd7b.chunk.js"
+  },
+  {
+    "revision": "e8329f14f9a185fab076",
+    "url": "/duel-game/static/js/runtime-main.ca3dc0ef.js"
+  }
+]);
